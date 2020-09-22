f1376fbf17a74cbd5f52505ac77913c16dec9513
Ashton <10988492+ashco@users.noreply.github.com>
2020-09-21 23:07:22 -0700

    Creating attributes for randomize request.

--- server.js
+++ server.js
@@ -2,79 +2,83 @@
 
 const express = require("express");
 const v3 = require("node-hue-api").v3;
+const bodyParser = require("body-parser");
 
 const { genRandomHue } = require("./lib/helpers");
 
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
 const LightState = v3.lightStates.LightState;
 
-const USERNAME = process.env.USERNAME,
-  // The name of the light we wish to retrieve by name
-  LIGHT_ID = 11; // 8, 9, 11
+const PORT = process.env.PORT || 9000;
+const USERNAME = process.env.USERNAME;
 
-v3.discovery
-  // connect to bridge
-  .nupnpSearch()
-  .then((searchResults) => {
-    const host = searchResults[0].ipaddress;
-    return v3.api.createLocal(host).connect(USERNAME);
-  })
-  .then((api) => {
-    // Using a LightState object to build the desired state
-    const state = new LightState().on().ct(200).brightness(100);
-    return api.lights.setLightState(LIGHT_ID, state);
-  })
-  .then((result) => {
-    console.log(`Light state change was successful? ${result}`);
-  });
+/**
+ * optBag {
+ *   username: req
+ *   lights: [1, 3, 5],
+ *   group: 1
+ *   lightGroups: [[1, 2], [4]]
+ * }
+ * */
 
-app.get("/randomize", async (req, res, next) => {
-  // console.log(`I'm so random! ${Math.random()}`);
-  // try {
-  // } catch {
-  // }
-  v3.discovery
-    // connect to bridge
-    .nupnpSearch()
-    .then((searchResults) => {
-      const host = searchResults[0].ipaddress;
-      return v3.api.createLocal(host).connect(USERNAME);
-    })
-    .then((api) => {
-      // Using a LightState object to build the desired state
-      // const state = new LightState()
-      //   .on()
-      //   .brightness(100)
-      //   .saturation(100)
-      //   .hue(genRandomHue())
-      //   .alertShort();
-      // return api.lights.setLightState(LIGHT_ID, state);
-      const sideState = new LightState()
-        .on()
-        .brightness(100)
-        .saturation(100)
-        .hue(genRandomHue())
-        .alertShort();
-      const centerState = new LightState()
-        .on()
-        .brightness(100)
-        .saturation(100)
-        .hue(genRandomHue())
-        .alertShort();
-      api.lights.setLightState(8, sideState);
-      api.lights.setLightState(9, sideState);
-      return api.lights.setLightState(11, centerState);
-    })
-    .then((result) => {
-      console.log(`Light state change was successful? ${result}`);
-    });
+app.post("/randomize", async (req, res, next) => {
+  try {
+    const { username, lights, group, lightGroups } = req.body;
+    // console.log(JSON.stringify(req.body));
+    v3.discovery
+      // connect to bridge
+      .nupnpSearch()
+      .then((searchResults) => {
+        const host = searchResults[0].ipaddress;
+        return v3.api.createLocal(host).connect(username);
+      })
+      .then((api) => {
+        // Using a LightState object to build the desired state
+        if (lights) {
+          lights.split(",").forEach((light) => {
+            const state = new LightState()
+              .on()
+              .brightness(100)
+              .saturation(100)
+              .hue(genRandomHue());
 
-  res.status(200).send();
+            api.lights.setLightState(parseInt(light), state);
+          });
+        } else if (group) {
+          console.log(group);
+        } else if (lightGroups) {
+          console.log(lightGroups);
+        }
+
+        // const sideState = new LightState()
+        //   .on()
+        //   .brightness(100)
+        //   .saturation(100)
+        //   .hue(genRandomHue())
+        //   .alertShort();
+        // const centerState = new LightState()
+        //   .on()
+        //   .brightness(100)
+        //   .saturation(100)
+        //   .hue(genRandomHue())
+        //   .alertShort();
+        // api.lights.setLightState(8, sideState);
+        // api.lights.setLightState(9, sideState);
+        // return api.lights.setLightState(11, centerState);
+      });
+
+    res.status(200).send();
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
-const port = process.env.PORT || 9000;
-
-app.listen(port, () => {
-  console.log(`Hue Randomizer Server running on port http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Hue Randomizer Server running on port http://localhost:${PORT}`);
 });
